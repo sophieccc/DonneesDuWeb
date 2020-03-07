e1aa83faba9aa79dc05379fa19da7826bf2d6e70
Sophie Crowley <crowleso@tcd.ie>
2020-03-07 14:18:18 +0000

    Setup

---
+++ Sujet_TP_DW_2020/fichiers/ajax/partieJavascript.js
@@ -0,0 +1,123 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function recupererPremierEnfantDeTypeNode(n) {
+    var x = n.firstChild;
+    while (x.nodeType != 1) { // Test if x is an element node (and not a text node or other)
+        x = x.nextSibling;
+    }
+    return x;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//change le contenu de l'élement avec l'id "nom" avec la chaine de caractéres en paramètre	  
+function setNom(nom) {
+    var elementHtmlARemplir = window.document.getElementById("id_nom_a_remplacer");
+    elementHtmlARemplir.innerHTML = nom;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//charge le fichier XML se trouvant à l'URL relative donné dans le paramètreet le retourne
+function chargerHttpXML(xmlDocumentUrl) {
+
+    var httpAjax;
+
+    httpAjax = window.XMLHttpRequest ?
+        new XMLHttpRequest() :
+        new ActiveXObject('Microsoft.XMLHTTP');
+
+    if (httpAjax.overrideMimeType) {
+        httpAjax.overrideMimeType('text/xml');
+    }
+
+    //chargement du fichier XML à l'aide de XMLHttpRequest synchrone (le 3° paramètre est défini à false)
+    httpAjax.open('GET', xmlDocumentUrl, false);
+    httpAjax.send();
+
+    return httpAjax.responseXML;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  
+// Charge le fichier JSON se trouvant à l'URL donnée en paramètre et le retourne
+function chargerHttpJSON(jsonDocumentUrl) {
+
+    var httpAjax;
+
+    httpAjax = window.XMLHttpRequest ?
+        new XMLHttpRequest() :
+        new ActiveXObject('Microsoft.XMLHTTP');
+
+    if (httpAjax.overrideMimeType) {
+        httpAjax.overrideMimeType('text/xml');
+    }
+
+    // chargement du fichier JSON à l'aide de XMLHttpRequest synchrone (le 3° paramètre est défini à false)
+    httpAjax.open('GET', jsonDocumentUrl, false);
+    httpAjax.send();
+
+    var responseData = eval("(" + httpAjax.responseText + ")");
+
+    return responseData;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Bouton2_ajaxBibliographie(xmlDocumentUrl, xslDocumentUrl, newElementName) {
+
+    var xsltProcessor = new XSLTProcessor();
+
+    // Chargement du fichier XSL à l'aide de XMLHttpRequest synchrone 
+    var xslDocument = chargerHttpXML(xslDocumentUrl);
+
+    // Importation du .xsl
+    xsltProcessor.importStylesheet(xslDocument);
+
+    // Chargement du fichier XML à l'aide de XMLHttpRequest synchrone 
+    var xmlDocument = chargerHttpXML(xmlDocumentUrl);
+
+    // Création du document XML transformé par le XSL
+    var newXmlDocument = xsltProcessor.transformToDocument(xmlDocument);
+
+    // Recherche du parent (dont l'id est "here") de l'élément à remplacer dans le document HTML courant
+    var elementHtmlParent = window.document.getElementById("id_element_a_remplacer");
+    // Premier élément fils du parent
+    var elementHtmlARemplacer = recupererPremierEnfantDeTypeNode(elementHtmlParent);
+    // Premier élément "elementName" du nouveau document (par exemple, "ul", "table"...)
+    var elementAInserer = newXmlDocument.getElementsByTagName(newElementName)[0];
+
+    // Remplacement de l'élément
+    elementHtmlParent.replaceChild(elementAInserer, elementHtmlARemplacer);
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Bouton3_ajaxEmployees(xmlDocumentUrl) {
+
+
+    var xmlDocument = chargerHttpXML(xmlDocumentUrl);
+
+    //extraction des noms à partir du document XML (avec une feuille de style ou en javascript)
+    var lesNoms = xmlDocument.getElementsByTagName("LastName");
+
+    // Parcours de la liste des noms avec une boucle for et 
+    // construction d'une chaine de charactères contenant les noms séparés par des espaces 
+    // Pour avoir la longueur d'une liste : attribut 'length'
+    // Accès au texte d'un noeud "LastName" : NOM_NOEUD.firstChild.nodeValue
+    var chaineDesNoms = "";
+    for (i = 0; i < lesNoms.length; i++) {
+        if (i > 0) {
+            chaineDesNoms = chaineDesNoms + ", ";
+        }
+        chaineDesNoms = chaineDesNoms + lesNoms[i].firstChild.nodeValue + " ";
+    }
+
+
+    // Appel (ou recopie) de la fonction setNom(...) ou bien autre façon de modifier le texte de l'élément "span"
+    setNom(chaineDesNoms);
+
+
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Bouton4_ajaxEmployeesTableau(xmlDocumentUrl, xslDocumentUrl) {
+    //commenter la ligne suivante qui affiche la boîte de dialogue!
+    alert("Fonction à compléter...");
+}
---
+++ Sujet_TP_DW_2020/fichiers/pays_du_monde_tableau_3IF.xsl
@@ -0,0 +1,81 @@
+<?xml version = "1.0" encoding = "UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output method="html" />
+    <xsl:template match="/"> 
+        <html> 
+            <head> 
+                <title> 
+                Pays du monde 
+            </title> 
+            </head> 
+            <body style="background-color:white;">
+                <h1>Les pays du monde</h1>
+                <xsl:apply-templates select="//metadonnees"/>
+                <xsl:apply-templates select="//countries"/>
+            </body>
+        </html> 
+    </xsl:template> 
+    <xsl:template match="metadonnees">
+        <p style="text-align:center; color:blue;">
+            Objectif : <xsl:value-of select="objectif"/>
+        </p><hr/>
+    </xsl:template>
+
+    <xsl:template match="countries">
+        <table border="3" width="100%" align="center">
+        <xsl:apply-templates select="//country"/>
+        </table>
+    </xsl:template>
+
+    <xsl:template match="country"> 
+        <tr>
+        <td>
+        <xsl:value-of select="count(preceding-sibling::country)+1"/>
+        </td>
+            <td>
+                <span style="color:green;"><xsl:value-of select="name/common"/></span>
+                <span> (<xsl:value-of select="name/official"/>)</span>
+                <xsl:if test="name/native_name/@lang='eng'">
+                    <br/>
+                    <span style="color:blue;">
+                        Nom anglais: <xsl:value-of select="name/native_name[@lang='eng']/official"/>
+                    </span>        
+                </xsl:if> 
+            </td>
+            <td>
+            <xsl:value-of select="capital"/>
+            </td>
+            <td>
+                <xsl:choose>
+                    <xsl:when test="borders">
+                        <xsl:apply-templates select="borders"/>
+                    </xsl:when>
+                    <xsl:otherwise>
+                    <xsl:if test="landlocked='false'">
+                        Île
+                    </xsl:if>
+                    </xsl:otherwise>
+                </xsl:choose>
+            </td>
+            <td>
+            Longitude: <xsl:value-of select="coordinates/@long"/>
+            <br/>
+            Latitude: <xsl:value-of select="coordinates/@lat"/>
+            </td>
+            <td>
+            <xsl:variable name="lowercase" select="'abcdefghijklmnopqrstuvwxyz'" />
+            <xsl:variable name="uppercase" select="'ABCDEFGHIJKLMNOPQRSTUVWXYZ'" />
+            <img src="http://www.geonames.org/flags/x/{translate(codes/cca2, $uppercase, $lowercase)}.gif" 
+            alt="" height="40" width="60"/> 
+            </td>
+        </tr>
+    </xsl:template>
+
+    <xsl:template match="borders"> 
+        <xsl:for-each select="neighbour">
+        <xsl:value-of select="//country[codes/cca3=current()]/name/common"/>
+        <xsl:if test="position() != last()">, 
+        </xsl:if>
+        </xsl:for-each>
+    </xsl:template>
+</xsl:stylesheet>
